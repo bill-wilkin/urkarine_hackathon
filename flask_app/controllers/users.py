ddbd471ebbc7e644356cad9888fb7092a597aed3
bree parker <breannaparker1991@gmail.com>
2022-06-26 20:06:53 -0600

    added some user code

---
+++ flask_app/controllers/users.py
@@ -0,0 +1,9 @@
+import os
+from flask import render_template, redirect, flash, request, session
+from flask_app import app
+from flask_app.models.user import User
+from flask_app.models.resource import Resource
+from flask_app.models.representative import Representative
+
+
+@app.route('/')
