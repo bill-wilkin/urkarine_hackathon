6024d5525b3809bd7c6cbcd8f57a8df63b3fce9a
KeithLion <nortonkiara6@gmail.com>
2022-06-26 20:47:38 -0700

    edited routes, styled the resource and representatives page

--- flask_app/controllers/resources.py
+++ flask_app/controllers/resources.py
@@ -4,26 +4,30 @@ from flask import render_template, redirect, session, request, flash
 from flask_app.models.representative import Representative
 from ..models import user, resource
 import requests
-import os 
+import os
 from dotenv import load_dotenv
 import json
 load_dotenv()
 
 # renders page for all rources
+
+
 @app.route('/resources')
 def all_resources():
-    return render_template("<<Insert html title here>>")
+    return render_template("resources.html")
 
 # renders page for adding a new rource
+
+
 @app.route('/resources/new')
 def new_resources():
     if "user_id" not in session:
         return redirect("/")
-    return render_template("<<Insert html title here>>")
+    return render_template("new_resource.html")
 
 
 # creates a new resource
-@app.route("/resources/create", methods = ["POST"])
+@app.route("/resources/create", methods=["POST"])
 def create_resource():
     if "user_id" not in session:
         return redirect("/")
@@ -38,7 +42,7 @@ def create_resource():
 def edit_resource(resource_id):
     if "user_id" not in session:
         return redirect("/")
-    return render_template("<<Insert html title here>>", user = user.user.get_by_id({"id":session['user_id']}), resource = resource.resource.get_by_id({"id":resource_id}))
+    return render_template("edit_resource.html", user=user.user.get_by_id({"id": session['user_id']}), resource=resource.resource.get_by_id({"id": resource_id}))
 
 
 # updates resource
@@ -46,22 +50,22 @@ def edit_resource(resource_id):
 def update_resource(resource_id):
     if "user_id" not in session:
         return redirect("/")
-    user = user.user.get_by_id({"id":session['user_id']})
-    resource = resource.resource.get_by_id({"id":resource_id})
+    user = user.user.get_by_id({"id": session['user_id']})
+    resource = resource.resource.get_by_id({"id": resource_id})
     if user.id != resource.user.id:
         return redirect("/")
 
-    resource.Resource.update({"id":resource_id})
+    resource.Resource.update({"id": resource_id})
     return redirect(f"resources/{resource_id}/edit")
 
 # deletes a resource
+
+
 @app.route('/resouces/<resource_id>/destroy')
 def destroy_resource(resource_id):
-    user = user.user.get_by_id({"id":session['user_id']})
-    resource = resource.resource.get_by_id({"id":resource_id})
+    user = user.user.get_by_id({"id": session['user_id']})
+    resource = resource.resource.get_by_id({"id": resource_id})
     if user.id != resource.user.id or user.id != 1:
         return redirect("/")
-    resource.Resource.destroy({"id":resource_id})
+    resource.Resource.destroy({"id": resource_id})
     return redirect(f"resources/")
-
-
